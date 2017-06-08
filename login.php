3b3f54bbdc2ae05c737464bb042093c3b2c2d2be
Steven <skeith123943@gmail.com>
2017-06-08 21:06:37 +0200

    Update login.php

--- login.php
+++ login.php
@@ -33,41 +33,41 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
 
 ?>
 
-<html lang="fr">
+<!DOCTYPE html>
+<html>
 <head>
-
-    <title>DevEnvGen</title>
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-
+	<meta charset="UTF-8"/>
+	<title>DevEnv</title>
+	<link rel="stylesheet" type="text/css" href="http://bootswatch.com/flatly/bootstrap.css">
 </head>
 <body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/index.php">DevEnv Generator</a>
+      </div>
+    </div>
+  </nav>
 
-  <form action="index.php" method="POST">
-      <div class="login">
-	       <div class="login-screen">
-	          <div class="app-title">
-		            <h1>DevEnvGen Login</h1>
-	          </div>
-
-            <div class="login-form">
-              <div class="control-group">
-                <input type="text" class="login-field" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>" placeholder="identifiant" name="identifiant">
-                <label class="login-field-icon fui-user" for="login-name"></label>
-              </div>
-
-              <div class="control-group">
-                <input type="password" class="login-field" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>" placeholder="mot de passe" name="mot de passe">
-                <label class="login-field-icon fui-lock" for="login-pass"></label>
-              </div>
-
-              <input type="submit" name="connexion" value="Connexion" class="btn btn-primary btn-block" >
-              <?php
-              if (isset($erreur)) echo '<br /><br />',$erreur;
-              ?>
-              <br>
-            </div>
-          </div>
-        </div>
-      </form>
+<div class="jumbotron">
+	<fieldset>
+    <legend>DevEnvGen Login</legend>
+    <div class="form-group">
+      <label for="inputEmail" class="col-lg-2 control-label"></label>
+      <div class="col-lg-10">
+        <input type="text" class="form-control" id="inputEmail" placeholder="Identifiant">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="inputPassword" class="col-lg-2 control-label"></label>
+      <div class="col-lg-10">
+        <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
+			</div>
+    </div>
+</div>
 </body>
 </html>
