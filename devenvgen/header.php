e57f898b6d41fe898192364f65a6446353bd2fdf
Steven <skeith123943@gmail.com>
2017-06-08 16:08:00 +0200

    Add files via upload

---
+++ devenvgen/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>DevEnv</title>
+	<link rel="stylesheet" type="text/css" href="http://bootswatch.com/flatly/bootstrap.css">
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">DevEnv Generator</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="container.php">Vos DevEnv<span class="sr-only">(current)</span></a></li>
+          <li><a href="add_container.php">Créer un DevEnv</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="disconnect.php">Déconnexion</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<div class="container">
