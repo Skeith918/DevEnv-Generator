6a2b9757f6d05cf78e81bb04d4f3b9a5486d6d43
Steven <skeith123943@gmail.com>
2017-06-09 10:59:54 +0200

    Add files via upload

--- env/env.php
+++ env/env.php
@@ -23,8 +23,13 @@
   </thead>
   <tbody>
     <?php
-    include "searchenv.php";
+    //include "searchenv.php";
     for ($i = 1; $i <= 5; $i++) {
+      $name = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f12 | head -n' .$i. '' );
+      $os = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f16 | head -n' .$i. '' );
+      $tools = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f20 | head -n' .$i. '' );
+      $ip = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f24 | head -n' .$i. '' );
+      $port = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f28 | head -n' .$i. '' );
       echo '<html>
       <tr>
         <td>';echo $i;echo'</td>
