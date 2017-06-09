2eb1cf6c0f95ace035d5a7801e341807c61f26ed
Steven <skeith123943@gmail.com>
2017-06-09 11:13:28 +0200

    Add files via upload

--- env/env.php
+++ env/env.php
@@ -21,30 +21,35 @@
       <th>Port Ouverts</th>
     </tr>
   </thead>
-  <tbody>
-    <?php
-    //include "searchenv.php";
-    for ($i = 1; $i <= 5; $i++) {
-      $name = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f12 | head -n' .$i. '' );
-      $os = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f16 | head -n' .$i. '' );
-      $tools = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f20 | head -n' .$i. '' );
-      $ip = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f24 | head -n' .$i. '' );
-      $port = shell_exec('echo "db.container.find({owner : "' .$owner. '"});" | mongo localhost:27017/devenvgen --quiet | cut -d ' .echo '"'. ' -f28 | head -n' .$i. '' );
-      echo '<html>
-      <tr>
-        <td>';echo $i;echo'</td>
-        <td>';echo $name;echo'</td>
-        <td>';echo $os;echo'</td>
-        <td>';echo $tools;echo'</td>
-        <td>';echo $ip;echo'</td>
-        <td>';echo $port;echo'</td>
-        <td><a href="delenv.php?name=';echo $name;echo'" class="btn btn-danger btn-xs">Supprimer</a></td>
-      </tr>
-      </html>';
-    }
 
-    ?>
-  </tbody>
+    <tbody>
+      <?php
+      //include "searchenv.php";
+      $owner='ssnoussi';
+      $separator='"';
+      for ($i = 1; $i <= 5; $i++) {
+        $name = shell_exec('/bin/echo "db.container.find({owner : ' .$owner. '});" | /usr/bin/mongo localhost:27017/devenvgen --quiet | cut -d ' .$separator. ' -f12 | head -n' .$i. '' );
+        $os = shell_exec('/bin/echo "db.container.find({owner : ' .$owner. '});" | /usr/bin/mongo localhost:27017/devenvgen --quiet | cut -d ' .$separator. ' -f16 | head -n' .$i. '' );
+        $tools = shell_exec('/bin/echo "db.container.find({owner : ' .$owner. '});" | /usr/bin/mongo localhost:27017/devenvgen --quiet | cut -d ' .$separator. ' -f20 | head -n' .$i. '' );
+        $ip = shell_exec('/bin/echo "db.container.find({owner : ' .$owner. '});" | /usr/bin/mongo localhost:27017/devenvgen --quiet | cut -d ' .$separator. ' -f24 | head -n' .$i. '' );
+        $port = shell_exec('/bin/echo "db.container.find({owner : ' .$owner. '});" | /usr/bin/mongo localhost:27017/devenvgen --quiet | cut -d ' .$separator. ' -f28 | head -n' .$i. '' );
+        echo '<html>
+        <tr>
+          <td>';echo $i;echo'</td>
+          <td>';echo $name;echo'</td>
+          <td>';echo $os;echo'</td>
+          <td>';echo $tools;echo'</td>
+          <td>';echo $ip;echo'</td>
+          <td>';echo $port;echo'</td>
+          <td><a href="delenv.php?name=';echo $name;echo'" class="btn btn-danger btn-xs">Supprimer</a></td>
+        </tr>
+        </html>';
+      }
+
+      ?>
+    </tbody>
+
+
 
 </body>
 </html>
